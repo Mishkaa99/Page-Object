1b39c3b0b6a8d22ea66133e7fe36b15a072eb4f1
Миша Дмитриев <Dmitriev_m.a@mail.ru>
2021-10-05 21:10:04 +0500

    :)

--- conftest.py
+++ conftest.py
@@ -1,30 +1,19 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-#По выбору:
-#Запускать: pytest -s -v --browser_name=chrome test_parser.py
-#def pytest_addoption(parser):
-#    parser.addoption('--browser_name', action='store', default=None,
-#                     help="Choose browser: chrome or firefox")
 
-#По умолчанию chrome:
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default="chrome",
-                 help="Choose browser: chrome or firefox")
-
+    parser.addoption('--language', action='store', default=None,
+                 help="Choose browser: language")
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    user_language = request.config.getoption("language")
+    print("\nstart chrome browser for test..")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -6,26 +6,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-@pytest.fixture(scope="function")
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
 
-@pytest.mark.parametrize('namber', ["236895", "236896", "236897", "236898", "236899", "236903", "236904", "236905"])
-def test_guest_should_see_login_link(browser, namber):
-    link = f"https://stepik.org/lesson/{namber}/step/1"
+
+def test_guest_should_see_login_link(browser):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    browser.implicitly_wait(5)
-    browser.find_element_by_tag_name("textarea").send_keys(str(math.log(int(time.time()))))   
-    time.sleep(0.3)
-    browser.find_element_by_class_name("submit-submission").click()   
+    browser.find_element_by_xpath("//*[@id='add_to_basket_form']/button").click()   
     # находим элемент, содержащий текст
-    welcome_text_elt = browser.find_element_by_tag_name("pre")
+    welcome_text_elt = browser.find_element_by_xpath("//*[@id='messages']/div[1]/div/strong")
     # записываем в переменную welcome_text текст из элемента welcome_text_elt
     welcome_text = welcome_text_elt.text
     # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта   
-    assert("Correct!" == welcome_text), print("Послание:"+ welcome_text)
+    assert("Coders at Work" == welcome_text), print("Ошибка:"+ welcome_text)
     time.sleep(10)
