72a22cefd7c7e157d2f22bf78f36d3633854f2de
Миша Дмитриев <Dmitriev_m.a@mail.ru>
2021-10-05 21:21:12 +0500

    :)

--- test_items.py
+++ test_items.py
@@ -11,7 +11,7 @@ from selenium.webdriver.common.by import By
 def test_guest_should_see_login_link(browser):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(30)
+    time.sleep(5)
     browser.find_element_by_xpath("//*[@id='add_to_basket_form']/button").click()   
     # находим элемент, содержащий текст
     welcome_text_elt = browser.find_element_by_xpath("//*[@id='messages']/div[1]/div/strong")
